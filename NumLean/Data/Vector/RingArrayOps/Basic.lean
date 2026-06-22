fa701c01c677c0d0c5d5c78111a726c7525d2c5e
Tomas Skrivan <skrivantomas@seznam.cz>
2026-06-22 08:11:37 +0200

    ...

--- NumLean/Data/Vector/RingArrayOps/Basic.lean
+++ NumLean/Data/Vector/RingArrayOps/Basic.lean
@@ -7,6 +7,8 @@ namespace NumLean
 
 namespace Vector
 
+local macro_rules | `(tactic| get_elem_tactic_extensible) => `(tactic| tbounds)
+
 set_option backward.do.legacy false
 
 def axpyRef [Add K] [Mul K] {xn yn : Nat} (n : Nat)
@@ -15,15 +17,15 @@ def axpyRef [Add K] [Mul K] {xn yn : Nat} (n : Nat)
     (hy : yoff + n * yinc ≤ yn ∧ yinc ≠ 0) : Vector K yn := Id.run do
   let mut ys := ys
   for_all h : i in 0...n do
-    ys[yoff + i * yinc]'(by tbounds) :=
-      ys[yoff + i * yinc]'(by tbounds) + a * xs[xoff + i * xinc]'(by tbounds)
+    ys[yoff + i * yinc] :=
+      ys[yoff + i * yinc] + a * xs[xoff + i * xinc]
   return ys
 
 def scalRef [Mul K] {xn : Nat} (n : Nat) (a : K) (xs : Vector K xn) (xoff xinc : Nat)
     (hx : xoff + n * xinc ≤ xn ∧ xinc ≠ 0) : Vector K xn := Id.run do
   let mut xs := xs
   for_all h : i in 0...n do
-    xs[xoff + i * xinc]'(by tbounds) := a * xs[xoff + i * xinc]'(by tbounds)
+    xs[xoff + i * xinc] := a * xs[xoff + i * xinc]
   return xs
 
 def mulRef [Mul K] {xn yn : Nat} (n : Nat)
@@ -32,8 +34,8 @@ def mulRef [Mul K] {xn yn : Nat} (n : Nat)
     (hy : yoff + n * yinc ≤ yn ∧ yinc ≠ 0) : Vector K yn := Id.run do
   let mut ys := ys
   for_all h : i in 0...n do
-    ys[yoff + i * yinc]'(by tbounds) :=
-      ys[yoff + i * yinc]'(by tbounds) * xs[xoff + i * xinc]'(by tbounds)
+    ys[yoff + i * yinc] :=
+      ys[yoff + i * yinc] * xs[xoff + i * xinc]
   return ys
 
 def dotRef [Add K] [Mul K] [Zero K] {xn yn : Nat} (n : Nat)
@@ -42,14 +44,14 @@ def dotRef [Add K] [Mul K] [Zero K] {xn yn : Nat} (n : Nat)
     (hy : yoff + n * yinc ≤ yn ∧ yinc ≠ 0) : K := Id.run do
   let mut r := 0
   for_all h : i in 0...n do
-    r := r + xs[xoff + i * xinc]'(by tbounds) * ys[yoff + i * yinc]'(by tbounds)
+    r := r + xs[xoff + i * xinc] * ys[yoff + i * yinc]
   return r
 
 def sumRef [Add K] [Zero K] {xn : Nat} (n : Nat) (xs : Vector K xn) (xoff xinc : Nat)
     (hx : xoff + n * xinc ≤ xn ∧ xinc ≠ 0) : K := Id.run do
   let mut r := 0
   for_all h : i in 0...n do
-    r := r + xs[xoff + i * xinc]'(by tbounds)
+    r := r + xs[xoff + i * xinc]
   return r
 
 end Vector
