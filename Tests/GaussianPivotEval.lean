61764f999672e1343f4fa6d4619eba314159df4f
Tomas Skrivan <skrivantomas@seznam.cz>
2026-06-28 08:42:00 +0200

    ...

--- Tests/GaussianPivotEval.lean
+++ Tests/GaussianPivotEval.lean
@@ -10,13 +10,13 @@ open NumLean
 namespace Tests.GaussianPivotEval
 
 def a0 : FloatArray :=
-  [0.0, 2.0,
+  [0.1, 2.0,
    1.0, 3.0].toFloatArray
 
 def b0 : FloatArray :=
   [4.0, 5.0].toFloatArray
 
-/-- info: ([1.000000, 3.000000, 0.000000, 2.000000], [5.000000, 4.000000]) -/
+/-- info: ([1.000000, 3.000000, 0.000000, 1.700000], [5.000000, 3.500000]) -/
 #guard_msgs in
 #eval Tests.CCompiler.gaussianElimPartialPivot 2 a0 b0
 
